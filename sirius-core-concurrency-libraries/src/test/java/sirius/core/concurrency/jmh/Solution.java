12b2c7e8d9b37d0034779be37c28fadf48d1dfb2
shadow <1723928492@qq.com>
2023-06-05 18:08:21 +0800

    feat: 添加test

--- sirius-core-concurrency-libraries/src/test/java/sirius/core/concurrency/jmh/Solution.java
+++ sirius-core-concurrency-libraries/src/test/java/sirius/core/concurrency/jmh/Solution.java
@@ -1,20 +1,30 @@
 package sirius.core.concurrency.jmh;
 
 /**
+ * cpu核心数：https://zhuanlan.zhihu.com/p/86855590
  * @author shadow
  * @create 2023-06-04 23:18
  **/
 public class Solution {
 
     public static void main(String[] args) {
-        final String s1 = "123";
-        String s2 = "123";
-        /**
-         * s3是个final变量，那么只能指向123，不能指向其他的
-         */
-        final String s3 = new String("123");
-        //s3 = new String("234");s3是个final变量，那么只能指向123，不能指向其他的
-        String s4 = new String("123");
+        int availableProcessors = Runtime.getRuntime().availableProcessors();
+        System.out.println("Available Processors: " + availableProcessors);
+
+        for(int i = 0;i < availableProcessors;i++){
+            Thread t = new Thread(()-> {
+                while (true){
+                    try {
+                        int ps = Runtime.getRuntime().availableProcessors();
+                        Thread.sleep(1L);
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            t.start();
+        }
     }
 
 }
