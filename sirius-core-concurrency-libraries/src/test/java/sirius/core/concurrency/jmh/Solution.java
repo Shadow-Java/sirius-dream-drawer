42d6de572c78b21ba2a206e00f5499396fb5b2b7
shadow <1723928492@qq.com>
2023-06-07 15:51:55 +0800

    test: 添加thread

--- sirius-core-concurrency-libraries/src/test/java/sirius/core/concurrency/jmh/Solution.java
+++ sirius-core-concurrency-libraries/src/test/java/sirius/core/concurrency/jmh/Solution.java
@@ -8,22 +8,35 @@ package sirius.core.concurrency.jmh;
 public class Solution {
 
     public static void main(String[] args) {
+
+    }
+
+    public void getCurrentActiveThread(){
         int availableProcessors = Runtime.getRuntime().availableProcessors();
         System.out.println("Available Processors: " + availableProcessors);
+        ThreadGroup threadGroup = Thread.currentThread().getThreadGroup();
+        System.out.println("当前start线程数："+Thread.activeCount());
 
         for(int i = 0;i < availableProcessors;i++){
             Thread t = new Thread(()-> {
                 while (true){
                     try {
                         int ps = Runtime.getRuntime().availableProcessors();
+                        System.out.println("ps:"+ps);
                         Thread.sleep(1L);
                     }catch (Exception e){
                         e.printStackTrace();
                     }
                 }
-            });
+            },"thread-"+i);
 
             t.start();
+        }
+        System.out.println("end:"+Thread.activeCount());
+        Thread[] threads = new Thread[Thread.activeCount()];
+        threadGroup.enumerate(threads);
+        for (Thread thread:threads){
+            System.out.println("线程："+thread.getName());
         }
     }
 
