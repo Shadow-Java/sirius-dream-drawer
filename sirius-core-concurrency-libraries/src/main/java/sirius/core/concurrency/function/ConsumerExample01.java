a088294ff9a02e71263f0a2a7b86eb5ccd784f05
shadow <1723928492@qq.com>
2023-06-14 13:12:51 +0800

    docs:

--- sirius-core-concurrency-libraries/src/main/java/sirius/core/concurrency/function/ConsumerExample01.java
+++ sirius-core-concurrency-libraries/src/main/java/sirius/core/concurrency/function/ConsumerExample01.java
@@ -3,6 +3,10 @@ package sirius.core.concurrency.function;
 import java.util.function.Consumer;
 
 /**
+ * 函数式接口@FunctionalInterface：只包含一个抽象方法的接口，称为函数式接口；
+ * 箭头将Lambda表达式分为左右两部分，左边写的是实现的这个接口中的抽象方法中的形参列表，右边就是对抽象方法的处理；
+ *
+ * 函数式接口一般没有实现方法，即功能需要自己定义；Lambda表达式就是对函数式接口的一种简写方式，所以只有是函数式接口，我们才能用Lambda表达式；
  * @author shadow
  * @create 2023-06-11 19:04
  **/
